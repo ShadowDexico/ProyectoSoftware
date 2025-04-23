96f79a5a23c4b08d0f62f743f377a97d9295e617
ShadowDexico <fpertuz05@gmail.com>
2025-04-23 17:08:32 -0500

    subir el proyecto backend

---
+++ gestionrutas/src/main/java/com/fernandopertuz/gestionrutas/GestionrutasApplication.java
@@ -0,0 +1,13 @@
+package com.fernandopertuz.gestionrutas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionrutasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionrutasApplication.class, args);
+	}
+
+}
---
+++ gestionrutas/src/test/java/com/fernandopertuz/gestionrutas/GestionrutasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandopertuz.gestionrutas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionrutasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
